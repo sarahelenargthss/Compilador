faa012e7912b5a893e02a0c00f7471bdb84d29dc
sarahelenargthss <sarahelenargthss@gmail.com>
2020-04-17 20:58:13 -0300

    Finalizacoes das especificacoes do gals

---
+++ src/model/Constants.java
@@ -0,0 +1,56 @@
+package model;
+
+public interface Constants extends ScannerConstants
+{
+    int EPSILON  = 0;
+    int DOLLAR   = 1;
+
+    int t_identificador = 2;
+    int t_cInteira = 3;
+    int t_cReal = 4;
+    int t_cBinaria = 5;
+    int t_cHexadecimal = 6;
+    int t_cString = 7;
+    int t_bin = 8;
+    int t_bool = 9;
+    int t_def = 10;
+    int t_do = 11;
+    int t_elif = 12;
+    int t_else = 13;
+    int t_end = 14;
+    int t_false = 15;
+    int t_float = 16;
+    int t_hexa = 17;
+    int t_if = 18;
+    int t_int = 19;
+    int t_listen = 20;
+    int t_main = 21;
+    int t_speak = 22;
+    int t_str = 23;
+    int t_toInt = 24;
+    int t_toBin = 25;
+    int t_toHexa = 26;
+    int t_true = 27;
+    int t_whileFalse = 28;
+    int t_TOKEN_29 = 29; //"("
+    int t_TOKEN_30 = 30; //")"
+    int t_TOKEN_31 = 31; //"=="
+    int t_TOKEN_32 = 32; //"!="
+    int t_TOKEN_33 = 33; //"<"
+    int t_TOKEN_34 = 34; //">"
+    int t_TOKEN_35 = 35; //"&"
+    int t_TOKEN_36 = 36; //"|"
+    int t_TOKEN_37 = 37; //"!"
+    int t_TOKEN_38 = 38; //"+"
+    int t_TOKEN_39 = 39; //"-"
+    int t_TOKEN_40 = 40; //"*"
+    int t_TOKEN_41 = 41; //"/"
+    int t_TOKEN_42 = 42; //","
+    int t_TOKEN_43 = 43; //"."
+    int t_TOKEN_44 = 44; //";"
+    int t_TOKEN_45 = 45; //":"
+    int t_TOKEN_46 = 46; //"="
+    int t_TOKEN_47 = 47; //"+="
+    int t_TOKEN_48 = 48; //"-="
+
+}
